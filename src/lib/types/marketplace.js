d6d9e92f9d86f15cdc3859c62f55d209ae9e2c4e
easysalesexporthq <easysalesexporthq@gmail.com>
2026-05-17 21:27:02 +0100

    fix: remove compiled .js files that were overriding typescript logic in production

--- src/lib/types/marketplace.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-/**
- * Marketplace Type Definitions
- *
- * Complete type definitions for the Easy Sales Digital Marketplace
- */
-Object.defineProperty(exports, "__esModule", { value: true });
--- src/lib/types/prd-interfaces.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-/**
- * PRD-Required Type Interfaces
- * Added: 2026-02-06
- */
-Object.defineProperty(exports, "__esModule", { value: true });
